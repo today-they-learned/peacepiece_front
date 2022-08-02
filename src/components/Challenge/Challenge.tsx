7120ba7744e16c1ae99efbb1270cb7596bf1d887
cychann <skcbdlhc129@naver.com>
2022-08-02 22:43:06 +0900

    Feat: 완료/미완료 카드 컴포넌트 생성

--- src/components/Challenge/Challenge.tsx
+++ src/components/Challenge/Challenge.tsx
@@ -1,7 +1,13 @@
+import Card from "components/Card/Card";
 import React from "react";
 
 const Challenge = () => {
-  return <div>챌린지</div>;
+  return (
+    <>
+      <div>챌린지</div>
+      <Card />
+    </>
+  );
 };
 
 export default Challenge;
