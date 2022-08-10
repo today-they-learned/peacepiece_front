68dc5b2bb6340351efd79931fdb38811db847361
minjj0905 <minjj0905@kookmin.ac.kr>
2022-08-10 10:47:22 +0900

    refactor: delete unused var

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import COLOR from "constants/color";
 
 const Container = styled.div`
@@ -17,36 +16,6 @@ const Thumbnail = styled.img`
   height: 12rem;
   border-radius: 2rem 2rem 0 0;
   margin-bottom: 1rem;
-`;
-
-const CompleteThumb = styled.img`
-  width: 16.3rem;
-  height: 12rem;
-  border-radius: 2rem 2rem 0 0;
-  margin-bottom: 1rem;
-  filter: brightness(50%);
-`;
-
-const CompleteBox = styled.div`
-  position: absolute;
-  top: 1rem;
-  right: 1rem;
-  color: ${COLOR.font.primary};
-  width: 3.75rem;
-  height: 3.75rem;
-  border-radius: 50%;
-  background: ${COLOR.black};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CompleteWord = styled.span`
-  width: 2rem;
-  font-size: 0.875rem;
-  font-family: "Pr-SemiBold";
-  text-align: center;
-  line-height: 1rem;
 `;
 
 const ContenttBox = styled.div`
