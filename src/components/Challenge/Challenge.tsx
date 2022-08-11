f8a37e861f85023069f51a3edb93476898dbe133
cychann <skcbdlhc129@naver.com>
2022-08-11 18:14:37 +0900

    Feat: 지난 챌린지 레이아웃 구성

--- src/components/Challenge/Challenge.tsx
+++ src/components/Challenge/Challenge.tsx
@@ -2,13 +2,62 @@ import Card from "components/Card/Card";
 import EndedCard from "components/Card/EndedCard";
 import React from "react";
 
+import styled, { css } from "styled-components";
+
+const LayoutContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 73.56rem;
+  margin: 1rem auto;
+`;
+
+const Title = styled.div`
+  margin-bottom: 1.5rem;
+`;
+
+const CardContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
 const Challenge = () => {
   return (
-    <>
-      <div>챌린지</div>
-      <Card />
-      <EndedCard />
-    </>
+    <Container>
+      <Title>챌린지</Title>
+      <CardContainer>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <EndedCard />
+      </CardContainer>
+    </Container>
   );
 };
 
