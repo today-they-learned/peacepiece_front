33dd2d285b618ec506c117a10e3ae1c1fea89cc7
cychann <skcbdlhc129@naver.com>
2022-08-14 13:06:48 +0900

    Fix: 라우터에 맞게 파일 변경

--- src/components/Card/EndedCard.tsx
+++ src/components/Card/EndedCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import COLOR from "constants/color";
 
 const Container = styled.div`
--- src/components/Challenge/Challenge.tsx
+++ src/components/Challenge/Challenge.tsx
@@ -4,56 +4,12 @@ import React from "react";
 
 import styled, { css } from "styled-components";
 
-const Container = styled.div`
-  width: 73.56rem;
-  margin: 1rem auto;
-`;
-
 const Title = styled.div`
   margin-bottom: 1.5rem;
 `;
 
-const CardContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-`;
-
 const Challenge = () => {
-  return (
-    <Container>
-      <Title>챌린지</Title>
-      <CardContainer>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <EndedCard />
-      </CardContainer>
-    </Container>
-  );
+  return <Title>진행 중인 챌린지</Title>;
 };
 
 export default Challenge;
