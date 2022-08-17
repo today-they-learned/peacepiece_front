4c1e1a29f60c5eb8d9df8cd04535e720ba6cfa0d
cychann <66055587+cychann@users.noreply.github.com>
2022-08-17 13:43:37 +0900

    Featuer/34_ended_challenge_layout (#44)
    * Style: 카드 디자인 수정
    
    * Feat: 완료/미완료 카드 컴포넌트 분리
    
    * Fix: 불필요한 태그 제거
    
    * Feat: 지난 챌린지 레이아웃 구성
    
    * Fix: 오타 수정
    
    * Fix: 라우터에 맞게 파일 변경
    
    * Fix: subnav 스크롤바 포함 문제 수정

--- src/components/Challenge/Challenge.tsx
+++ src/components/Challenge/Challenge.tsx
@@ -1,13 +1,15 @@
 import Card from "components/Card/Card";
+import EndedCard from "components/Card/EndedCard";
 import React from "react";
 
+import styled, { css } from "styled-components";
+
+const Title = styled.div`
+  margin-bottom: 1.5rem;
+`;
+
 const Challenge = () => {
-  return (
-    <>
-      <div>진행 중인 챌린지</div>
-      <Card />
-    </>
-  );
+  return <Title>진행 중인 챌린지</Title>;
 };
 
 export default Challenge;
