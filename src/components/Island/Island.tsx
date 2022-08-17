410d3a7b665afa8cc172c272185cb330ed79b36d
soyekwon <soye0710@naver.com>
2022-08-18 04:42:23 +0900

    feat: tootip

--- src/components/Island/Island.tsx
+++ src/components/Island/Island.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import HoverTooltip from "components/Tooltip/Tooltip";
 
 const Island = () => {
-  return <div style={{ height: "60rem" }}>소개</div>;
+  return (
+    <div style={{ height: "60rem" }}>
+      <HoverTooltip
+        text="챌린지 인증을 하면 PP 포인트를 받을 수 있어요! 😎"
+        width="15rem"
+      />
+    </div>
+  );
 };
 
 export default Island;
--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,64 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { Icon } from "semantic-ui-react";
-import FlexBox from "components/common/FlexBox";
+import ReactTooltip from "react-tooltip";
 
 export interface Props {
-  children: React.ReactNode;
-  top: string;
-  left: string;
+  text: string;
+  width?: string;
 }
 
-const Container = styled.div`
-  width: 14rem;
-  height: 10rem;
-  display: flex;
-  flex-direction: column-reverse;
-  position: absolute;
-  top: ${(p: Props) => p.top};
-  left: ${(p: Props) => p.left};
-  z-index: 10;
-`;
-
-const Tooltip = styled.div`
-  position: relative;
-  margin-bottom: 0.25rem;
-  padding: 0.6rem 1.2rem 0.6rem 1.2rem;
-  background: black;
-  color: white;
-  font-size: 13px;
-  font-weight: bolder;
-  :after {
-    border-top: 0.9rem solid black;
-    border-left: 0.6rem solid transparent;
-    border-right: 0.6rem solid transparent;
-    border-bottom: 0rem solid transparent;
-    content: "";
-    position: absolute;
-    bottom: -0.6rem;
-    left: 1.4rem;
-  }
-`;
+const defaultProps = {
+  width: "auto",
+};
 
 const HoverTooltip = (props: Props) => {
-  const { children, top, left } = props;
-  const [hover, setHover] = useState(false);
+  const { text, width } = props;
+
+  const Tooltip = styled(ReactTooltip)`
+    width: ${width};
+    font-size: 13px;
+    font-family: "Pr-Bold";
+    background-color: black !important;
+    text-align: center;
+  `;
 
   return (
-    <Container top={top} left={left}>
-      <FlexBox margin="0 0 0 1rem">
-        <Icon
-          onMouseOver={() => setHover(true)}
-          onMouseLeave={() => setHover(false)}
-          name="question circle"
-          size="large"
-          color="grey"
-          link
-        />
-      </FlexBox>
-      {hover ? <Tooltip>{children}</Tooltip> : ""}
-    </Container>
+    <div style={{ padding: "20%" }}>
+      <Icon
+        name="question circle"
+        size="large"
+        color="grey"
+        link
+        data-tip="React-tooltip"
+      />
+      <Tooltip effect="solid">{text}</Tooltip>
+    </div>
   );
 };
+HoverTooltip.defaultProps = defaultProps;
 
 export default HoverTooltip;
