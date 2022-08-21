b544e2f25a8e55ada586d2b18e78775f8cb5c64a
soyekwon <soye0710@naver.com>
2022-08-21 19:21:09 +0900

    modal import 삭제

--- src/pages/Island.tsx
+++ src/pages/Island.tsx
@@ -1,17 +1,7 @@
 import React from "react";
-import ModalFrame from "components/Modal/ModalFrame";
-import COLOR from "constants/color";
 
 const Island = () => {
-  return (
-    <div>
-      <ModalFrame titleColor={COLOR.font.danger}>
-        <div style={{ color: "white" }}>
-          이것은 내용입니다. 블라블라 ~ 여기에 본문 내용을 ......{" "}
-        </div>
-      </ModalFrame>
-    </div>
-  );
+  return <div>내소개</div>;
 };
 
 export default Island;
