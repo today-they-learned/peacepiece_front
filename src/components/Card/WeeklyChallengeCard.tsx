b76c2a5cff4aecca8ac8ab831b54b9ad5b10c316
soyekwon <soye0710@naver.com>
2022-08-22 16:45:20 +0900

    flexTextBox활용

--- src/components/Card/WeeklyChallengeCard.tsx
+++ src/components/Card/WeeklyChallengeCard.tsx
@@ -1,16 +1,8 @@
 import React from "react";
-import styled from "styled-components";
 import COLOR from "constants/color";
 import FlexTextBox from "components/common/FlexTextBox";
 import BannerBox from "components/common/BannerBox";
 import ChallengeFigure from "components/common/ChallengeFigure";
-
-const Title = styled.div`
-  font-family: "Pr-SemiBold";
-  font-size: 1.25rem;
-  margin: 0.2rem 0 0.5rem 0;
-  color: ${COLOR.font.primary};
-`;
 
 const WeeklyChallengeCard = () => {
   return (
@@ -21,7 +13,13 @@ const WeeklyChallengeCard = () => {
       borderRadius="1.8rem"
       backgroundColor={COLOR.bg.secondary}
     >
-      <Title>일회용컵 No! 텀블러 Yes!</Title>
+      <FlexTextBox
+        fontSize="1.25rem"
+        margin="0.2rem 0 0.5rem 0"
+        color={COLOR.font.primary}
+      >
+        일회용컵 No! 텀블러 Yes!
+      </FlexTextBox>
 
       <ChallengeFigure person={10} point={100} />
       <FlexTextBox fontSize="1rem" margin="0.4rem 0 0 0">
