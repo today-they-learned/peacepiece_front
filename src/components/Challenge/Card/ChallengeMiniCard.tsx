da6771276ddeb83f221a37fbad5c3b280358c65c
soyekwon <soye0710@naver.com>
2022-08-23 22:04:25 +0900

    main merge 후 수정사항 적용

--- src/components/Challenge/Card/ChallengeMiniCard.tsx
+++ src/components/Challenge/Card/ChallengeMiniCard.tsx
@@ -2,28 +2,34 @@ import React from "react";
 import COLOR from "constants/color";
 import FlexTextBox from "components/common/FlexTextBox";
 import BannerBox from "components/common/BannerBox";
-import ChallengeFigure from "components/common/ChallengeFigure";
+import ChallengeFigure from "components/Challenge/ChallengeFigure";
 
-const WeeklyChallengeCard = () => {
+interface Props {
+  title: string;
+  content: string;
+}
+
+const WeeklyChallengeCard = (props: Props) => {
+  const { title, content } = props;
   return (
     <BannerBox
       width="21.4rem"
       height="9.25rem"
       padding="1.2rem 1.5rem 1.2rem 1.5rem"
-      borderRadius="1.8rem"
-      backgroundColor={COLOR.bg.secondary}
+      borderRadius="1.25rem"
+      theme="secondary"
     >
       <FlexTextBox
         fontSize="1.25rem"
         margin="0.2rem 0 0.5rem 0"
         color={COLOR.font.primary}
       >
-        일회용컵 No! 텀블러 Yes!
+        {title}
       </FlexTextBox>
 
       <ChallengeFigure person={10} point={100} />
       <FlexTextBox fontSize="1rem" margin="0.4rem 0 0 0">
-        일주일동안 음료를 마실 때, 텀블러를 이용하고 인증사진을 공유해주세요!
+        {content}
       </FlexTextBox>
     </BannerBox>
   );
