5215d4fbd2cc9f5cd34033dd06ff83a7521c0fc1
soyekwon <soye0710@naver.com>
2022-08-26 15:14:26 +0900

    캐러샐 완성

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,104 +1,54 @@
-import React from "react";
 import { useMediaQuery, Paper } from "@mui/material";
 import styled from "styled-components";
 import COLOR from "constants/color";
 import Carousel from "react-material-ui-carousel";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import ChallengeOfferCard from "components/Card/ChallengeOfferCard";
+import ChallengeOfferCard from "components/Challenge/Card/ChallengeOfferCard";
 import FlexBox from "components/common/FlexBox";
+import { FlexTextBox } from "components/common";
 import dummyData from "./dummyData";
 
 const Container = styled.div`
-  width: 54rem;
-  height: 18.5rem;
-  padding: 1rem;
-  border-radius: 1.4rem;
+  width: 52rem;
+  height: 17.25rem;
+  padding: 0.8rem;
+  border-radius: 1.25rem;
   background-color: ${COLOR.bg.primary};
   @media only screen and (max-width: 768px) {
-    width: 30rem;
+    width: 28rem;
+    padding-left: 0;
   }
 `;
 
 const Carousell = styled(Carousel)`
-  width: 51.5rem;
+  width: 50.5rem;
   height: 10.5rem;
   background-color: ${COLOR.bg.primary};
   @media only screen and (max-width: 768px) {
-    width: 28rem;
+    width: 27.5rem;
   }
-`;
-
-const Title = styled.div`
-  width: 20rem;
-  margin: 1.5rem 0 1.5rem 0;
-  height: 2rem;
-  font-size: 1.7rem;
-  font-family: "Pr-Bold";
-  color: white;
 `;
 
 const Page = styled(Paper)`
   width: 49rem;
-  margin-left: 1rem;
+  margin-left: 0.5rem;
   display: flex;
   border: none !important;
   background-color: ${COLOR.bg.primary} !important;
   @media only screen and (max-width: 768px) {
     width: 24rem;
-    margin-left: 1rem;
+    margin-left: 0.6rem;
   }
 `;
 
-const Slider = () => {
-  const isDesktop = useMediaQuery("(min-width: 768px)");
-
-  const arrLoop = () => {
-    const newArr = [];
-    for (let i = 0; i <= dummyData.length - 1; i += 2) {
-      newArr.push(
-        <Page>
-          {i === dummyData.length - 1 && dummyData.length % 2 === 1 ? (
-            <FlexBox margin="0 1rem 0 1rem">
-              <ChallengeOfferCard
-                userName={dummyData[i].userName}
-                content={dummyData[i].content}
-                like={dummyData[i].like}
-                clicked={dummyData[i].clicked}
-              />
-            </FlexBox>
-          ) : (
-            <>
-              <FlexBox margin="0 0.5rem 0 2rem">
-                <ChallengeOfferCard
-                  userName={dummyData[i].userName}
-                  content={dummyData[i].content}
-                  like={dummyData[i].like}
-                  clicked={dummyData[i].clicked}
-                />
-              </FlexBox>
-              <FlexBox margin="0 1rem 0 1rem">
-                <ChallengeOfferCard
-                  userName={dummyData[i + 1].userName}
-                  content={dummyData[i + 1].content}
-                  like={dummyData[i + 1].like}
-                  clicked={dummyData[i + 1].clicked}
-                />
-              </FlexBox>
-            </>
-          )}
-        </Page>
-      );
-    }
-    return newArr;
-  };
-
-  const arrLoopMobile = () => {
-    const newArr = [];
-    for (let i = 0; i < dummyData.length; i += 1) {
-      newArr.push(
-        <Page>
-          <FlexBox margin="0 0.5rem 0 2rem">
+const arrLoop = () => {
+  const newArr = [];
+  for (let i = 0; i <= dummyData.length - 1; i += 2) {
+    newArr.push(
+      <Page>
+        {i === dummyData.length - 1 && dummyData.length % 2 === 1 ? (
+          <FlexBox margin="0 0.8rem 0 0.8rem">
             <ChallengeOfferCard
               userName={dummyData[i].userName}
               content={dummyData[i].content}
@@ -106,17 +56,61 @@ const Slider = () => {
               clicked={dummyData[i].clicked}
             />
           </FlexBox>
-        </Page>
-      );
-    }
-    return newArr;
-  };
+        ) : (
+          <>
+            <FlexBox margin="0 0.5rem 0 2.2rem">
+              <ChallengeOfferCard
+                userName={dummyData[i].userName}
+                content={dummyData[i].content}
+                like={dummyData[i].like}
+                clicked={dummyData[i].clicked}
+              />
+            </FlexBox>
+            <FlexBox margin="0 1rem 0 0.5rem">
+              <ChallengeOfferCard
+                userName={dummyData[i + 1].userName}
+                content={dummyData[i + 1].content}
+                like={dummyData[i + 1].like}
+                clicked={dummyData[i + 1].clicked}
+              />
+            </FlexBox>
+          </>
+        )}
+      </Page>
+    );
+  }
+  return newArr;
+};
 
-  const insertData = isDesktop ? arrLoop() : arrLoopMobile();
+const arrLoopMobile = () => {
+  const newArr = [];
+  for (let i = 0; i < dummyData.length; i += 1) {
+    newArr.push(
+      <Page>
+        <FlexBox margin="0 0.5rem 0 2rem">
+          <ChallengeOfferCard
+            userName={dummyData[i].userName}
+            content={dummyData[i].content}
+            like={dummyData[i].like}
+            clicked={dummyData[i].clicked}
+          />
+        </FlexBox>
+      </Page>
+    );
+  }
+  return newArr;
+};
+
+const Slider = () => {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  const insertCard = isDesktop ? arrLoop() : arrLoopMobile();
 
   return (
     <Container>
-      <Title>📝 이번주 챌린지</Title>
+      <FlexTextBox fontSize="1.7rem" margin="1.4rem 0 1.4rem 0.8rem">
+        📝 이번주 챌린지
+      </FlexTextBox>
       <Carousell
         height="11rem"
         animation="slide"
@@ -133,7 +127,7 @@ const Slider = () => {
           },
         }}
       >
-        {insertData}
+        {insertCard}
       </Carousell>
     </Container>
   );
--- src/pages/Island.tsx
+++ src/pages/Island.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Slider from "components/Slider/Slider";
 
 const Island = () => {
-  return (
-    <div>
-      <Slider />
-    </div>
-  );
+  return <div>내소개</div>;
 };
 
 export default Island;
