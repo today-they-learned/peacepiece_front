f73e0f77923275844110754d2b4490b1538a7464
SoyE <55877726+soyekwon@users.noreply.github.com>
2022-08-31 23:50:48 +0900

    Feature/65 person icon (#82)
    * style: img -> icon 변경
    
    * refactor: FlexBox, FlexTextBox 활

--- src/components/Challenge/ChallengeFigure.tsx
+++ src/components/Challenge/ChallengeFigure.tsx
@@ -1,49 +1,42 @@
 import styled from "styled-components";
 import COLOR from "constants/color";
+import { FlexBox, FlexTextBox } from "components/common";
+import { AiOutlineUser } from "react-icons/ai";
 
 interface Props {
   person: number;
   point: number;
 }
 
-const Figure = styled.div`
-  display: flex;
-  margin-right: 1.2rem;
-`;
-
-const Personal = styled.div`
-  display: flex;
-  margin-right: 1rem;
-`;
-
-const Person = styled.img`
+const Person = styled(AiOutlineUser)`
   width: 1.1rem;
   height: 1rem;
-  margin: 0.1rem 0.2rem 0 0;
-`;
-
-const Number = styled.span`
-  font-size: 0.75rem;
-  font-family: "Pr-Medium";
-  color: ${COLOR.font.secondary};
-`;
-
-const Point = styled.span`
-  font-size: 0.875rem;
-  font-family: "Pr-Medium";
+  margin: 0.2rem 0.2rem 0 0;
   color: ${COLOR.font.secondary};
 `;
 
 const ChallengeFigure = (props: Props) => {
   const { person, point } = props;
   return (
-    <Figure>
-      <Personal>
-        <Person src={`${process.env.PUBLIC_URL}/images/person.png`} />
-        <Number>{person}명</Number>
-      </Personal>
-      <Point>PP {point}</Point>
-    </Figure>
+    <FlexBox margin="0 1.2rem 0 0">
+      <FlexBox margin="0 1rem 0 0">
+        <Person />
+        <FlexTextBox
+          fontSize="0.75rem"
+          fontFamily="Pr-Medium"
+          color={COLOR.font.secondary}
+        >
+          {person}명
+        </FlexTextBox>
+      </FlexBox>
+      <FlexTextBox
+        fontSize="0.875rem"
+        fontFamily="Pr-Medium"
+        color={COLOR.font.secondary}
+      >
+        PP {point}
+      </FlexTextBox>
+    </FlexBox>
   );
 };
 
