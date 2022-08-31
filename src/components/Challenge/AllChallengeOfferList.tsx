8c504a5961e05a4c44c873e0d1b020c991b1436d
cychann <skcbdlhc129@naver.com>
2022-08-31 21:19:31 +0900

    Feat: 챌린지 제안 모아보기 컴포넌트 틀 구현

---
+++ src/components/Challenge/AllChallengeOfferList.tsx
@@ -0,0 +1,33 @@
+import { FlexBox } from "components/common";
+import ChallengeOfferCard from "./Card/ChallengeOfferCard";
+
+interface Props {
+  challenges: Array<{
+    id: number;
+    userName: string;
+    content: string;
+    like: number;
+    clicked: boolean;
+  }>;
+}
+
+const AllChallengeOfferList = (props: Props) => {
+  const { challenges } = props;
+  return (
+    <FlexBox wrap="wrap" margin="2rem 0 0 0">
+      {challenges.map((challenge) => {
+        return (
+          <ChallengeOfferCard
+            key={challenge.id}
+            userName={challenge.userName}
+            content={challenge.content}
+            like={challenge.like}
+            clicked={challenge.clicked}
+          />
+        );
+      })}
+    </FlexBox>
+  );
+};
+
+export default AllChallengeOfferList;
