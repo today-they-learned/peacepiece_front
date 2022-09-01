24294b17006e4121a06efdcf1cbd0cb2795e610d
cychann <skcbdlhc129@naver.com>
2022-09-01 16:25:59 +0900

    Refactor: 챌린지 타이틀 배경색 설정 인자 추가

--- src/components/Challenge/ChallengeTitle.tsx
+++ src/components/Challenge/ChallengeTitle.tsx
@@ -1,22 +1,25 @@
 import Tooltip from "components/Tooltip/Tooltip";
 import { FlexBox, FlexTextBox } from "components/common";
+import COLOR from "constants/color";
 
 interface Props {
   title: string;
   toolTipContent?: string;
   margin?: string;
+  background?: string;
 }
 
 const defaultProps = {
   toolTipContent: "",
   margin: "0.1rem 0 0 0",
+  background: COLOR.bg.default,
 };
 
 const ChallengeTitle = (props: Props) => {
-  const { title, toolTipContent, margin } = props;
+  const { title, toolTipContent, margin, background } = props;
 
   return (
-    <FlexBox alignItems="center">
+    <FlexBox alignItems="center" background={background}>
       <FlexTextBox fontSize="1.56rem" fontFamily="Pr-Bold" margin={margin}>
         {title}
       </FlexTextBox>
