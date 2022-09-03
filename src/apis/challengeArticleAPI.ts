138c3ad67876cc44751ec61a3bfef98394a171ca
minjj0905 <minjj0905@kookmin.ac.kr>
2022-09-04 06:27:13 +0900

    🔇 remove testcode

--- src/apis/challengeArticleAPI.ts
+++ src/apis/challengeArticleAPI.ts
@@ -2,9 +2,7 @@ import { instance } from "config/api";
 
 const challengeArticleAPI = {
   get: (challengeId: string, page: number) => {
-    return instance.get(
-      `articles/?challenge=${challengeId}&page=${page}&page_size=1`
-    );
+    return instance.get(`articles/?challenge=${challengeId}&page=${page}`);
   },
 };
 
