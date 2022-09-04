342ebf027cefef8ea3b28116ef0e2d2520150117
soyekwon <soye0710@naver.com>
2022-09-05 01:59:47 +0900

    style: 안 쓰는 코드 삭제

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,3 @@
-export type { default as CommentsType } from "./comments";
-export type { default as CommentType } from "./comment";
 export type { default as ImageType } from "./image";
 export type { default as ArticleType } from "./article";
 export type { default as ChallengeType } from "./challenge";
