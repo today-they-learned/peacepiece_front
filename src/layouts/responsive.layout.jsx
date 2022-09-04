f8d7d42984957ecdd2d7291017178392314338cf
minjj0905 <minjj0905@kookmin.ac.kr>
2022-09-04 23:53:43 +0900

    🎨 refator: layout code 구조 개선

--- src/layouts/responsive.layout.jsx
+++ src/layouts/responsive.layout.jsx
@@ -2,20 +2,20 @@ import { useMediaQuery } from "react-responsive";
 import DesktopLayout from "./desktop.layout";
 import MobileLayout from "./mobile.layout";
 
-const ResponsiveLayout = ({ children }) => {
-  const Desktop = () => {
-    const isDesktop = useMediaQuery({ minWidth: 768 });
-    return isDesktop && <DesktopLayout>{children}</DesktopLayout>;
-  };
-  const Mobile = () => {
-    const isMobile = useMediaQuery({ maxWidth: 767 });
-    return isMobile && <MobileLayout>{children}</MobileLayout>;
-  };
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+  return isDesktop && <DesktopLayout>{children}</DesktopLayout>;
+};
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return isMobile && <MobileLayout>{children}</MobileLayout>;
+};
 
+const ResponsiveLayout = ({ children }) => {
   return (
     <>
-      <Desktop />
-      <Mobile />
+      <Desktop>{children}</Desktop>
+      <Mobile>{children} </Mobile>
     </>
   );
 };
