3fcf7d63260323d7e868ae3e5431c3eff7eaa686
soyekwon <soye0710@naver.com>
2022-09-05 17:07:41 +0900

    feat: 글자 수 세기

--- src/components/Challenge/Banner/ChallengeOfferInput.tsx
+++ src/components/Challenge/Banner/ChallengeOfferInput.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import { FlexBox, FlexButton, FlexTextBox } from "components/common";
-import Input from "components/Form/Textarea";
 import COLOR from "constants/color";
+import styled from "styled-components";
 import ChallengeBanner from "../ChallengeBanner";
 
 const script = {
@@ -8,10 +9,24 @@ const script = {
   text: "어떤 챌린지가 PeacePiece에서 열리면 좋을까요? 다양한 생각을 공유해주세요",
 };
 
+const Textarea = styled.textarea`
+  width: 72rem !important;
+  height: 15rem !important;
+  border-radius: 1.25rem !important;
+  border: none;
+  padding: 1.25rem;
+  font-size: 1rem;
+  font-family: "Pr-Bold";
+  color: white !important;
+  background-color: ${COLOR.bg.secondary} !important;
+  margin: 1rem 0 0 0;
+`;
+
 const ChallengeOfferInput = () => {
+  const [cnt, setCnt] = useState(0);
   return (
     <ChallengeBanner title={script.title} width="76rem">
-      <Input width="72rem" text={script.text} minRow={6} margin="1rem 0 0 0" />
+      <Textarea id="text" onChange={(e) => setCnt(e.target.value.length)} />
       <FlexBox
         justifyContent="space-between"
         alignItems="center"
@@ -23,7 +38,7 @@ const ChallengeOfferInput = () => {
           fontFamily="Pr-Bold"
           color={COLOR.font.disabled}
         >
-          글자 수 10 / 100
+          글자 수 {cnt} / 100
         </FlexTextBox>
         <FlexButton
           fontSize="1.25rem"
