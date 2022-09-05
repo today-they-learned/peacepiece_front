08a968500e163770b2d56e7e3a42455d4a367088
cychann <66055587+cychann@users.noreply.github.com>
2022-09-05 12:49:23 +0900

    Feature/98 글 검색하기 배너 + 글 검색결과 배너 (#122)
    * feat: 글 검색하기 컴포넌트 제작
    
    * feat: 검색 결과 컴포넌트 제작
    
    * feat: piece 검색결과 컴포넌트 제작

---
+++ src/components/Challenge/Banner/PieceSearchBanner.tsx
@@ -0,0 +1,38 @@
+import { FlexBox } from "components/common";
+import styled from "styled-components";
+import COLOR from "constants/color";
+import { HiOutlineSearch } from "react-icons/hi";
+import ChallengeBanner from "../ChallengeBanner";
+
+const script = {
+  title: "글 검색하기",
+};
+
+const SearchInput = styled.input`
+  width: 22rem;
+  height: 2.875rem;
+  background-color: ${COLOR.bg.secondary};
+  border-radius: 1rem;
+  margin-top: 1rem;
+  padding: 1rem;
+`;
+
+const SearchBtn = styled.button`
+  position: absolute;
+  right: 0.5rem;
+  bottom: 0.5rem;
+`;
+
+const PieceSearchBanner = () => {
+  return (
+    <ChallengeBanner title={script.title} width="24.6rem">
+      <FlexBox position="relative" alignItems="center">
+        <SearchInput placeholder="ex) 일회용품" />
+        <SearchBtn>
+          <HiOutlineSearch color={COLOR.font.primary} size="25" />
+        </SearchBtn>
+      </FlexBox>
+    </ChallengeBanner>
+  );
+};
+export default PieceSearchBanner;
