349f7ba8f33f281de57f846d6555ba4a6359842d
shinkeonkim <singun11@kookmin.ac.kr>
2022-09-10 01:13:35 +0900

    fix: gap이 빠진 부분 수정

--- src/pages/ChallengeSuggestion.tsx
+++ src/pages/ChallengeSuggestion.tsx
@@ -9,6 +9,7 @@ const ChallengeSuggestion = () => {
       justifyContent="center"
       alignItems="center"
       margin="3rem 0 5rem 0 "
+      gap="1rem"
     >
       <ChallengeSuggestionInput />
       <AllChallengeSuggestion />
