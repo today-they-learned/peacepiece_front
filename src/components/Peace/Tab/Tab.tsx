bb6d48e0ccdbb022b786c600d58ece0d7334f484
shinkeonkim <singun11@kookmin.ac.kr>
2022-09-09 23:07:05 +0900

    refactor: 컴포넌트 이름을 가독성을 고려하여 수정

--- src/components/Peace/Tab/Tab.tsx
+++ src/components/Peace/Tab/Tab.tsx
@@ -1,10 +1,10 @@
-import { Tab } from "semantic-ui-react";
+import { Tab as semanticTab } from "semantic-ui-react";
 import COLOR from "constants/color";
 import { BannerBox } from "components/common";
 import styled from "styled-components";
 import { Island, Animal, Item, Point } from "./index";
 
-const TabPanee = styled(Tab.Pane)`
+const TabPane = styled(semanticTab.Pane)`
   height: 42rem;
   margin: 0 !important;
   padding: 0.6rem !important;
@@ -13,7 +13,7 @@ const TabPanee = styled(Tab.Pane)`
   border: none !important;
 `;
 
-const TabLib = styled(Tab)`
+const TabLib = styled(semanticTab)`
   width: 17.5rem;
   margin: 0 !important;
   background: ${COLOR.bg.nav} !important;
@@ -34,29 +34,29 @@ const panes = [
   {
     menuItem: "상점",
     render: () => (
-      <TabPanee>
+      <TabPane>
         <Island />
         <Animal />
         <Item />
-      </TabPanee>
+      </TabPane>
     ),
   },
   {
     menuItem: "포인트 이력",
     render: () => (
-      <TabPanee>
+      <TabPane>
         <Point />
-      </TabPanee>
+      </TabPane>
     ),
   },
 ];
 
-const Tabb = () => {
+const Tab = () => {
   return (
-    <BannerBox width="25rem" padding="1.05rem">
+    <BannerBox padding="1.05rem">
       <TabLib panes={panes} />
     </BannerBox>
   );
 };
 
-export default Tabb;
+export default Tab;
