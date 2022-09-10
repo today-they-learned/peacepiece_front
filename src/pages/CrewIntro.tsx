d1a0788493ce107a39bdc8f0e768f3153eb801eb
soyekwon <soye0710@naver.com>
2022-09-11 01:37:25 +0900

    feat: 1/6로 나누기

--- src/pages/CrewIntro.tsx
+++ src/pages/CrewIntro.tsx
@@ -48,42 +48,13 @@ const dummyData = [
   },
 ];
 
-interface Props {
-  gap?: string;
-}
-
-const defaultProps = {
-  gap: "2rem",
-};
-
-// const Img = styled.img`
-//   flex: 1 1 calc((100% - 2rem) / 3);
-//   max-width: calc((100% - 2rem) / 3);
-//   height: auto;
-//   max-height: 9.3rem;
-//   object-fit: cover;
-// `;
-const CrewIntroContainer = styled.div<{ gap: string }>`
-  width: 100%;
-  display: flex;
-  justify-content: flex-start;
-  max-width: 100%;
-  margin-left: 0.5rem;
-  display: flex;
-  border: none !important;
-  gap: 1rem;
-  padding: 1rem;
-  flex-direction: row;
-`;
-
 const Img = styled.img`
-  flex: 1 1 calc((100% - 1rem) / 6);
-  max-width: calc((100% - 1rem) / 6);
-  height: auto;
-  max-height: 9.3rem;
-  border-radius: 50%;
+  width: 100%;
+  height: 11rem;
+  border-radius: 20%;
   background: #d9d9d9;
   cursor: pointer;
+  object-fit: cover;
 `;
 const ContentContainer = styled.div`
   display: flex;
@@ -101,43 +72,73 @@ const Profile = styled.div`
   background: #d9d9d9;
   cursor: pointer;
 `;
+const Div = styled.div`
+  flex: 1 1 calc((100% - 2rem) / 6);
+  max-width: calc((100% - 2rem) / 6);
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (max-width: 700px) {
+    flex: 1 1 calc((100% - 2rem) / 3);
+    max-width: calc((100% - 2rem) / 3);
+  }
+`;
 
-const CrewIntro = ({ gap }: Props) => {
+const CrewIntro = () => {
   const [idx, setIdx] = useState(0);
   return (
     <FlexBox column gap="2rem">
-      <CrewIntroContainer gap={gap}>
-        <FlexBox width="100%" column center gap="0.4rem">
-          <Img onClick={() => setIdx(0)} />
+      <FlexBox gap="2rem">
+        <Div>
+          <Img
+            onClick={() => setIdx(0)}
+            src={`${process.env.PUBLIC_URL}/images/image2.png`}
+          />
           <FlexTextBox fontFamily="Pr-Regular">팀 리더</FlexTextBox>
           <FlexTextBox fontFamily="Pr-Regular">김신건</FlexTextBox>
-        </FlexBox>
-        <FlexBox width="100%" column center gap="0.4rem">
-          <Img onClick={() => setIdx(1)} />
+        </Div>
+        <Div>
+          <Img
+            onClick={() => setIdx(1)}
+            src={`${process.env.PUBLIC_URL}/images/image2.png`}
+          />
           <FlexTextBox fontFamily="Pr-Regular">팀원</FlexTextBox>
           <FlexTextBox fontFamily="Pr-Regular">권소예</FlexTextBox>
-        </FlexBox>
-        <FlexBox width="100%" column center gap="0.4rem">
-          <Img onClick={() => setIdx(2)} />
+        </Div>
+        <Div>
+          <Img
+            onClick={() => setIdx(2)}
+            src={`${process.env.PUBLIC_URL}/images/image2.png`}
+          />
           <FlexTextBox fontFamily="Pr-Regular">팀원</FlexTextBox>
           <FlexTextBox fontFamily="Pr-Regular">김민정</FlexTextBox>
-        </FlexBox>
-        <FlexBox width="100%" column center gap="0.4rem">
-          <Img onClick={() => setIdx(3)} />
+        </Div>
+        <Div>
+          <Img
+            onClick={() => setIdx(3)}
+            src={`${process.env.PUBLIC_URL}/images/image2.png`}
+          />
           <FlexTextBox fontFamily="Pr-Regular">팀원</FlexTextBox>
           <FlexTextBox fontFamily="Pr-Regular">진승희</FlexTextBox>
-        </FlexBox>
-        <FlexBox width="100%" column center gap="0.4rem">
-          <Img onClick={() => setIdx(4)} />
+        </Div>
+        <Div>
+          <Img
+            onClick={() => setIdx(4)}
+            src={`${process.env.PUBLIC_URL}/images/image2.png`}
+          />
           <FlexTextBox fontFamily="Pr-Regular">팀원</FlexTextBox>
           <FlexTextBox fontFamily="Pr-Regular">최지현</FlexTextBox>
-        </FlexBox>
-        <FlexBox width="100%" column center gap="0.4rem">
-          <Img onClick={() => setIdx(5)} />
+        </Div>
+        <Div>
+          <Img
+            onClick={() => setIdx(5)}
+            src={`${process.env.PUBLIC_URL}/images/image2.png`}
+          />
           <FlexTextBox fontFamily="Pr-Regular">팀원</FlexTextBox>
           <FlexTextBox fontFamily="Pr-Regular">최유찬</FlexTextBox>
-        </FlexBox>
-      </CrewIntroContainer>
+        </Div>
+      </FlexBox>
       <FlexBox
         width="100%"
         padding="2rem"
@@ -167,6 +168,4 @@ const CrewIntro = ({ gap }: Props) => {
     </FlexBox>
   );
 };
-
-CrewIntro.defaultProps = defaultProps;
 export default CrewIntro;
