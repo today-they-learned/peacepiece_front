7e5a5234aa3707eb91152e2011b72a7cebd2646b
minjj0905 <minjj0905@kookmin.ac.kr>
2022-09-12 05:12:17 +0900

    fix: piece 반응형 수정

--- src/components/Challenge/Banner/PieceSearchResult.tsx
+++ src/components/Challenge/Banner/PieceSearchResult.tsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 
 const PrimaryText = styled.span`
   color: ${COLOR.font.primary};
-  margin: 0 0.5rem;
 `;
 
 interface Props {
@@ -15,9 +14,9 @@ interface Props {
 
 const PieceSearchResult = ({ keyword, numOfPiece, isFetched }: Props) => {
   return (
-    <BannerBox width="100%" height="4.875rem" padding="1.5rem" margin="0">
+    <BannerBox width="100%" margin="0">
       {isFetched ? (
-        <FlexTextBox fontSize="1.56rem" fontFamily="Pr-Bold">
+        <FlexTextBox fontSize="1.56rem" fontFamily="Pr-Bold" lineHeight="2rem">
           {numOfPiece ? (
             <>
               <PrimaryText>{keyword}</PrimaryText>에 대한 검색결과입니다. &nbsp;
