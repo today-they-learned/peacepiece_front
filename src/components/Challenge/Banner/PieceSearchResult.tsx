98df6a5a8ad19d703ef72a5db4ab3a7417fd84dd
cychann <skcbdlhc129@naver.com>
2022-09-13 02:17:49 +0900

    fix: 피스 검색 로딩 스피너 적용

--- src/components/Challenge/Banner/PieceSearchResult.tsx
+++ src/components/Challenge/Banner/PieceSearchResult.tsx
@@ -1,5 +1,6 @@
 import { BannerBox, FlexTextBox } from "components/common";
 import COLOR from "constants/color";
+import Loading from "pages/Loading";
 import styled from "styled-components";
 
 const PrimaryText = styled.span`
@@ -14,28 +15,31 @@ interface Props {
 
 const PieceSearchResult = ({ keyword, numOfPiece, isFetched }: Props) => {
   return (
-    <BannerBox width="100%" margin="0">
-      {isFetched ? (
-        <FlexTextBox fontSize="1.56rem" fontFamily="Pr-Bold" lineHeight="2rem">
-          {numOfPiece ? (
-            <>
-              <PrimaryText>{keyword}</PrimaryText>에 대한 검색결과입니다. &nbsp;
-              (총
-              <PrimaryText>{numOfPiece}</PrimaryText>개의 글)
-            </>
-          ) : (
-            <>
-              <PrimaryText>😥 {keyword}</PrimaryText>에 대한 검색결과가
-              없습니다.
-            </>
-          )}
-        </FlexTextBox>
-      ) : (
-        <FlexTextBox fontSize="1.56rem" fontFamily="Pr-Bold">
-          검색중...
-        </FlexTextBox>
-      )}
-    </BannerBox>
+    <>
+      <BannerBox width="100%" margin="0">
+        {isFetched && (
+          <FlexTextBox
+            fontSize="1.56rem"
+            fontFamily="Pr-Bold"
+            lineHeight="2rem"
+          >
+            {numOfPiece ? (
+              <>
+                <PrimaryText>{keyword}</PrimaryText>에 대한 검색결과입니다.
+                &nbsp; (총
+                <PrimaryText>{numOfPiece}</PrimaryText>개의 글)
+              </>
+            ) : (
+              <>
+                <PrimaryText>😥 {keyword}</PrimaryText>에 대한 검색결과가
+                없습니다.
+              </>
+            )}
+          </FlexTextBox>
+        )}
+      </BannerBox>
+      {!isFetched && <Loading />}
+    </>
   );
 };
 
