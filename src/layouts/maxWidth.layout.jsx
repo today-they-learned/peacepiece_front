c0e8afc5486121de42524c16c138f9ee7c8d8d45
shinkeonkim <singun11@kookmin.ac.kr>
2022-09-14 23:57:22 +0900

    design: footer를 위한 가상 영역 추가

--- src/layouts/maxWidth.layout.jsx
+++ src/layouts/maxWidth.layout.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Container = styled.div`
   max-width: ${(props) => props.maxWidth || "1300px"};
   padding: ${(props) => props.padding || "0 1rem"};
+  min-height: 80vh;
   width: 100%;
   display: flex;
   flex-direction: column;
